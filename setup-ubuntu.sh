b68c1dcac00d23d47a5687b6ecca1f67c88e2967
fahmiludin <fahmi.ludin@gmail.com>
2022-10-25 12:21:32 +0800

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -2,10 +2,10 @@
 
 # Setup Docker & Docker Compose
 sudo apt-get update && sudo apt-get dist-upgrade -y
-sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker ${USER}
 
@@ -14,7 +14,7 @@ sudo curl -SL "https://github.com/docker/compose/releases/download/v2.10.2/docke
 sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
 # Setup AWS CLI
-sudo apt install unzip
+sudo apt install unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
