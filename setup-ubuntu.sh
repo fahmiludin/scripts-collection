fbfd273c91ad71003c5043d0a60e01ab0c6670e6
fahmiludin <fahmi.ludin@gmail.com>
2022-11-17 08:02:38 +0800

    start teleport on boot

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -22,6 +22,7 @@ source /etc/os-release
 echo "deb [signed-by=/usr/share/keyrings/teleport-archive-keyring.asc] https://apt.releases.teleport.dev/${ID?} ${VERSION_CODENAME?} stable/v11" | sudo tee /etc/apt/sources.list.d/teleport.list > /dev/null
 sudo apt-get update
 sudo apt-get install teleport -y
+sudo systemctl enable teleport
 
 # Clean up
 rm -rf aws awscliv2.zip
